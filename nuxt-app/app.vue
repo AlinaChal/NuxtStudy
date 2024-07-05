26438f7fed463ce9ab78b23c6f137be38e75e03a
AlinaChal <146351412+AlinaChal@users.noreply.github.com>
2024-07-05 15:10:09 +0300

    0507
    Страница с кнопками

--- nuxt-app/app.vue
+++ nuxt-app/app.vue
@@ -1,6 +1,34 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="wraper">
+   <h1>Welcome to the homepage</h1>
+   <p class="p">
+      Кнопка для нажатия
+      <button class="b">Send</button>
+   </p>
   </div>
 </template>
+
+
+<script setup lang="ts">
+</script>
+
+<style scoped lang="scss">
+  :root {
+    background-color: gray;
+  }
+  .wraper {
+    background-color: bisque;
+    border-radius: 1em;
+    border: 5px solid #900;
+    margin: 1em;
+    padding: 1em;
+
+    .p {
+      color: blue;
+
+      .b {
+        color: purple;
+      }
+    }
+  }
+</style>
