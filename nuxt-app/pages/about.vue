7ae6a59c0a7c4585f7f58b5ec63d065b87e2de00
AlinaChal <146351412+AlinaChal@users.noreply.github.com>
2024-07-09 16:10:14 +0300

    9 июля
    Какие-то курсы на ютюб на енглише, многостраничность тестила

---
+++ nuxt-app/pages/about.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+<h2>about</h2>
+<p>Text text text text</p>
+<p>Text text text text</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+  
+</script>
+
+<style lang="scss" scoped>
+h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+
+p {
+    margin: 20px 0;
+}
+</style>
