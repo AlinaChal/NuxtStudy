985ba7de54e8363bac86c842484117ec60f623d3
AlinaChal <146351412+AlinaChal@users.noreply.github.com>
2024-07-10 16:26:19 +0300

    10 июля
    Шапка сайта и чуть-чуть стилей

--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -12,11 +12,17 @@
 
 <style lang="scss" scoped>
 h2 {
+    background-color: #0b003dde;
     margin-bottom: 20px;
     font-size: 36px;
+    color: #ff06de;
+    border-radius: 1em;
+    border: 5px solid #b400ab8a;
+    padding: 1em;
 }
 
 p {
     margin: 20px 0;
+    color: #06c5ff;
 }
 </style>
